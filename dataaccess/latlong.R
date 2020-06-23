7f9b8050dbf65353dbd8394fd0a66d351c545229
paulapivat <paul.apivat@gmail.com>
2020-06-23 19:19:10 +0700

    do data preparation for latlong

--- dataaccess/latlong.R
+++ dataaccess/latlong.R
@@ -1,8 +1,16 @@
 # Convert UTM coordinate data into Longitude - Latitude data ----
 
 # Preparation ----
+install.packages('sp')
+library(data.table)
+library(sp)
+library(tidyverse)
+library(raster)
+
+load(file = 'saku.RData')
 
 # Data Manipulation ----
+
 
 # Visualization ----
 
