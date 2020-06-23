aa7bfafe8f53f02c34d43623a232e157cf7c558e
paulapivat <paul.apivat@gmail.com>
2020-06-23 22:50:13 +0700

    unable to convert utm to latlong

--- dataaccess/latlong.R
+++ dataaccess/latlong.R
@@ -33,7 +33,7 @@ proj4string(utm)
 # Thailand's ESPG code is 1231
 # source: http://www.epsg-registry.org/
 
-proj4string(utm) <- CRS("+init=epsg:2358")
+proj4string(utm) <- CRS("+init=epsg:1231")
 head(utm)
 
 # convert to Longitude-Latitude data
@@ -61,13 +61,60 @@ THAI.map %>%
     geom_map(map = THAI.map, 
              aes(x = long, y = lat, map_id = region),
              fill="white", colour = "black") +
-    coord_map()
+    coord_map() +
     geom_point(data = thai.map, 
                aes(x=lat, y=long, color="red", alpha = .9))
 
 # could not see red dots
 # use Bangkok Map from another project
-    
+library(rgdal)
+library(broom)
+
+# download BMASubDistrict_Polygon folder (whole folder) into working directory
+###### 180 SUBDISTRICTS
+# must contain: .shp, .shx, .dbf files
+# layer = is file name infront of .shp , .shx .dbf 
+bkk <- readOGR("./BMASubDistrict_Polygon", layer = 'BMA_ADMIN_SUB_DISTRICT')
+View(bkk)
+
+# quick plot in base R
+plot(bkk, col="#f2f2f2", bg="skyblue", lwd=0.25, border=0 )
+
+# IMPORTANT - to get longitude and lattitude you need convert to data frame
+# using broom package
+library(broom)
+
+bkk_fortified <- tidy(bkk)
+
+# more detailed plot with ggplot()
+ggplot() + geom_polygon(data = bkk_fortified, mapping = aes(x = long, y = lat, group = group), fill="#69b3a2", color="white", alpha = .5) + geom_point(data = thai.map, aes(x=long, y=lat, color="red"), inherit.aes = FALSE)  
+
+ggplot() + geom_point(data = thai.map, aes(x=lat, y=long, color="red")) 
+
+# What if original utm_x and utm_y were already in lat/long?
+thai.map$lat <- jobpost$utm_y
+thai.map$long <- jobpost$utm_x
+
+# ----- final conversion utm ----- #
+sputm <- SpatialPoints(utm, proj4string=CRS("+proj=utm +zone=47N +datum=WGS84"))  
+spgeo <- spTransform(sputm, CRS("+proj=longlat +datum=WGS84"))                     
+
+# transform again
+thai.map2 <- data.frame(Location = jobpost$location, 
+                       lat = spgeo$jobpost.utm_y, 
+                       long = spgeo$jobpost.utm_x)
+
+# thai map
+THAI.map %>%
+    ggplot() +
+    geom_map(map = THAI.map, 
+             aes(x = long, y = lat, map_id = region),
+             fill="white", colour = "black") +
+    coord_map() +
+    geom_point(data = thai.map2, 
+               aes(x=lat, y=long, color="red", alpha = .9))
+
+ggplot() + geom_polygon(data = bkk_fortified, mapping = aes(x = long, y = lat, group = group), fill="#69b3a2", color="white", alpha = .5) + geom_point(data = thai.map2, aes(x=lat, y=long, color="red", alpha = .9))
 
 # Conclusion ----
 thai.map %>% head()
