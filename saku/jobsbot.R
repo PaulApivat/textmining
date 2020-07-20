19e9fa19686f297fb93441b83e0b11f7775865b2
paulapivat <paul.apivat@gmail.com>
2020-07-20 12:49:34 +0700

    add more job classifications

--- saku/jobsbot.R
+++ saku/jobsbot.R
@@ -101,10 +101,35 @@ major_group_classification <- onet_job_classification %>%
 
 View(major_group_classification)
 
-# filter for restaurant-related
+# Impute Missing Values  ---- 
+# onet_job_classification
+# 23 Major Groups
 
-onet_job_classification %>% 
-    filter(`Major Group` == 35-0000) %>% view()
+onet_job_classification[1:94,1] <- '11-0000'
+onet_job_classification[95:170,1] <- '13-0000'
+onet_job_classification[171:223,1] <- '15-0000'
+onet_job_classification[224:307,1] <- '17-0000'
+onet_job_classification[308:404,1] <- '19-0000'
+onet_job_classification[405:431,1] <- '21-0000'
+onet_job_classification[432:446,1] <- '23-0000'
+onet_job_classification[447:547,1] <- '25-0000'
+onet_job_classification[548:613,1] <- '27-0000'
+onet_job_classification[614:740,1] <- '29-0000'
+onet_job_classification[741:770,1] <- '31-0000'
+onet_job_classification[771:817,1] <- '33-0000'
+onet_job_classification[818:851,1] <- '35-0000'
+onet_job_classification[852:869,1] <- '37-0000'
+onet_job_classification[870:931,1] <- '39-0000'
+onet_job_classification[932:975,1] <- '41-0000'
+onet_job_classification[976:1085,1] <- '43-0000'
+onet_job_classification[1086:1112,1] <- '45-0000'
+onet_job_classification[1113:1220,1] <- '47-0000'
+onet_job_classification[1221:1296,1] <- '49-0000'
+onet_job_classification[1297:1472,1] <- '51-0000'
+onet_job_classification[1473:1570,1] <- '53-0000'
+onet_job_classification[1571:1596,1] <- '55-0000'
+
+
 
 # Manual Classification ----
 
@@ -128,4 +153,130 @@ emp_jobpost[5,5] <- 'Customer Service Representatives'
 emp_jobpost[6,2:3] <- major_group_classification[1,1:2]
 emp_jobpost[6,4] <- '11-3120'
 emp_jobpost[6,5] <- 'Human Resources Managers'
+
+emp_jobpost[7,2:3] <- major_group_classification[17,1:2]
+emp_jobpost[7,4] <- '43-5070'
+emp_jobpost[7,5] <- 'Shipping, Receiving, and Inventory Clerks'
+
+emp_jobpost[8,2:3] <- major_group_classification[2,1:2]
+emp_jobpost[8,4] <- '13-2010'
+emp_jobpost[8,5] <- 'Accountants and Auditors'
+
+emp_jobpost[9,2:3] <- major_group_classification[4,1:2]
+emp_jobpost[9,4] <- '17-2050'
+emp_jobpost[9,5] <- 'Civil Engineers'
+    
+emp_jobpost[10,2:5] <- emp_jobpost[2,2:5]
+
+emp_jobpost[11,2:5] <- emp_jobpost[7,2:5]
+
+emp_jobpost[12,2:3] <- major_group_classification[16,1:2]
+emp_jobpost[12,4] <- '41-2030'
+emp_jobpost[12,5] <- 'Retail Salespersons'
+
+emp_jobpost[13,2:3] <- major_group_classification[17,1:2]
+emp_jobpost[13,4] <- '43-6010'
+emp_jobpost[13,5] <- 'Secretaries and Administrative Assistants'
+
+emp_jobpost[14,2:3] <- major_group_classification[2,1:2]
+emp_jobpost[14,4] <- '13-1160'
+emp_jobpost[14,5] <- 'Search Marketing Strategists'
+
+## after re-organize onet_job_classification
+
+emp_jobpost[15,2:3] <- major_group_classification[13,1:2]
+emp_jobpost[15,4] <- '35-3020'
+emp_jobpost[15,5] <- 'Fast Food and Counter Workers'
+
+emp_jobpost[16,2:3] <- major_group_classification[3,1:2]
+emp_jobpost[16,4] <- '15-1250'
+emp_jobpost[16,5] <- 'Software and Web Developers, Programmers, and Tester'
+
+emp_jobpost[17,2:5] <- emp_jobpost[2,2:5]
+
+emp_jobpost[18,2:3] <- major_group_classification[1,1:2]
+emp_jobpost[18,4] <- '11-2020'
+emp_jobpost[18,5] <- 'Marketing and Sales Managers'
+
+emp_jobpost[19,2:3] <- major_group_classification[2,1:2]
+emp_jobpost[19,4] <- '13-1080'
+emp_jobpost[19,5] <- 'Project Management Specialists'
+
+# skip row 20 emp_jobpost
+
+emp_jobpost[21,2:3] <- major_group_classification[20,1:2]
+emp_jobpost[21,4] <- '49-2090'
+emp_jobpost[21,5] <- 'Miscellaneous Electrical and Electronic Equipment Mechnics, Installers and Repairers'
+
+emp_jobpost[22,2:3] <- major_group_classification[18,1:2]
+emp_jobpost[22,4] <- '45-2020'
+emp_jobpost[22,5] <- 'Animal Breeders'
+
+
+
+emp_jobpost[23,2:3] <- major_group_classification[2,1:2]
+emp_jobpost[23,4] <- '13-1160'
+emp_jobpost[23,5] <- 'Search Marketing Strategists'
+
+
+emp_jobpost[25,2:3] <- major_group_classification[17,1:2]
+emp_jobpost[25,4] <- '43-5070'
+emp_jobpost[25,5] <- 'Shipping, Receiving, and Inventory Clerks'
+
+
+emp_jobpost[26,2:3] <- major_group_classification[16,1:2] 
+emp_jobpost[26,4] <- '41-3090'
+emp_jobpost[26,5] <- 'Miscellaneous Sales Representatives, Services'
+
+emp_jobpost[27,2:3] <- major_group_classification[22,1:2]
+emp_jobpost[27,4] <- '53-3030'
+emp_jobpost[27,5] <- 'Driver/Sales Workers and Truck Drivers'
+
+emp_jobpost[28,2:5] <- emp_jobpost[16,2:5]
+
+
+emp_jobpost[29,2:3] <- major_group_classification[3,1:2]
+emp_jobpost[29,4] <- '15-2030'
+emp_jobpost[29,5] <- 'Business Intelligence Analysts'
+
+emp_jobpost[30,2:3] <- major_group_classification[13,1:2]
+emp_jobpost[30,4] <- '35-3020'
+emp_jobpost[30,5] <- 'Baristas'
+
+emp_jobpost[31,2:3] <- major_group_classification[3,1:2] 
+emp_jobpost[31,4] <- '15-1240'
+emp_jobpost[31,5] <- 'Telecommunications Engineering Specialists'
+
+emp_jobpost[32,2:3] <- major_group_classification[1,1:2] 
+emp_jobpost[32,4] <- '11-1020'
+emp_jobpost[32,5] <- 'General and Operations Managers'
+
+# skip 33-34
+
+emp_jobpost[35,2:3] <- major_group_classification[19,1:2] 
+emp_jobpost[35,4] <- '47-4090'
+emp_jobpost[35,5] <- 'Miscellaneous Construction and Related Workers'
+
+emp_jobpost[36,2:3] <- major_group_classification[16,1:2]
+emp_jobpost[36,4] <- '41-2030'
+emp_jobpost[36,5] <- 'Retail Salespersons'
+
+emp_jobpost[26,2:3] 
+emp_jobpost[26,4]
+emp_jobpost[26,5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
