5a18613c33d4a91f0ec347212b81abde5ae3bd0e
paulapivat <paul.apivat@gmail.com>
2020-08-08 20:23:35 +0700

    add sandbox environment for function testing

--- saku/facebook_analytics/analysis.R
+++ saku/facebook_analytics/analysis.R
@@ -70,7 +70,7 @@ saku_page %>%
     head(n = 50)
 
 # Select 10 Columns of Interest
-saku_page %>%
+saku_page_select <- saku_page %>%
     select(Date, 
            `Lifetime Total Likes`, 
            `Daily Page Engaged Users`, 
@@ -80,13 +80,13 @@ saku_page %>%
            `28 Days Total Impressions`,
            `28 Days Organic impressions`,
            `28 Days Paid Impressions`,
-           `28 Days Viral impressions`) %>%
-    view()
+           `28 Days Viral impressions`)
 
 
 
 # visualize saku_page data ----
 library(lubridate)
+library(glue)
 
 # Lifetime Total Likes
 saku_page %>%
@@ -97,12 +97,98 @@ saku_page %>%
     mutate(Date = as.Date(Date) %>% ymd()) %>%
     
     ggplot(aes(x = Date, y = lifetime_total_likes)) +
-    scale_x_date(breaks = '2 day') +
+    geom_vline(xintercept = as.Date("2020-06-01"), na.rm = FALSE, linetype = 'dashed') +
+    geom_vline(xintercept = as.Date("2020-07-01"), na.rm = FALSE, linetype = 'dashed') +
+    geom_vline(xintercept = as.Date("2020-08-01"), na.rm = FALSE, linetype = 'dashed') +
+    scale_x_date(breaks = '5 day') +
     geom_line(color = 'red') +
+    theme_classic() +
     theme(
         axis.text.x = element_text(angle = 45, hjust = 1)
     )
+
+# create a function
+create_line_chart_fn<- function(dataset, col_name){
+    col_name <- enquo(col_name)
+    dataset %>%
+    select(Date, !!(col_name)) %>% 
+        mutate(as.character(!!(col_name)) = !!(col_name)) %>%
+        mutate(lifetime_total_likes = as.numeric(!!(col_name))) %>%
+        mutate(lifetime_total_likes = if_else(is.na(lifetime_total_likes), 0, lifetime_total_likes)) %>%
+        slice(-1) %>%
+        mutate(Date = as.Date(Date) %>% ymd()) %>%
+        
+        ggplot(aes(x = Date, y = lifetime_total_likes)) +
+        geom_vline(xintercept = as.Date("2020-06-01"), na.rm = FALSE, linetype = 'dashed') +
+        geom_vline(xintercept = as.Date("2020-07-01"), na.rm = FALSE, linetype = 'dashed') +
+        geom_vline(xintercept = as.Date("2020-08-01"), na.rm = FALSE, linetype = 'dashed') +
+        scale_x_date(breaks = '5 day') +
+        geom_line(color = 'green') +
+        theme_classic() +
+        theme(
+            axis.text.x = element_text(angle = 45, hjust = 1)
+        ) +
+        labs(
+            title = glue('Line Chart')
+        )
+}
+
+create_line_chart_fn(saku_page_select, `Daily Page Engaged Users`)
+
+
+
+#### Sandbox Functions ##########
+
+dataset %>%
+    select(Date, !!(col_name)) %>% 
+    mutate(as.character(!!(col_name)) = !!(col_name)) %>%
+    mutate(lifetime_total_likes = as.numeric(!!(col_name))) %>%
+    mutate(lifetime_total_likes = if_else(is.na(lifetime_total_likes), 0, lifetime_total_likes)) %>%
+    slice(-1) %>%
+    mutate(Date = as.Date(Date) %>% ymd()) %>%
     
+    ggplot(aes(x = Date, y = lifetime_total_likes)) +
+    geom_vline(xintercept = as.Date("2020-06-01"), na.rm = FALSE, linetype = 'dashed') +
+    geom_vline(xintercept = as.Date("2020-07-01"), na.rm = FALSE, linetype = 'dashed') +
+    geom_vline(xintercept = as.Date("2020-08-01"), na.rm = FALSE, linetype = 'dashed') +
+    scale_x_date(breaks = '5 day') +
+    geom_line(color = 'green') +
+    theme_classic() +
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1)
+    ) +
+    labs(
+        title = glue('Line Chart')
+    )
+
+##############################
+
+
+
+saku_page_select["Daily Page Engaged Users"]
+
+saku_page %>%
+    view()
+
+
+
+saku_page %>%
+    select(Date, `Lifetime Total Likes`) %>% 
+    mutate(lifetime_total_likes = as.numeric(`Lifetime Total Likes`)) %>%
+    mutate(lifetime_total_likes = if_else(is.na(lifetime_total_likes), 0, lifetime_total_likes)) %>%
+    slice(-1) %>%
+    mutate(Date = as.Date(Date) %>% ymd()) %>%
+    
+    ggplot(aes(x = Date, y = lifetime_total_likes)) +
+    geom_vline(xintercept = as.Date("2020-06-01"), na.rm = FALSE, linetype = 'dashed') +
+    geom_vline(xintercept = as.Date("2020-07-01"), na.rm = FALSE, linetype = 'dashed') +
+    geom_vline(xintercept = as.Date("2020-08-01"), na.rm = FALSE, linetype = 'dashed') +
+    scale_x_date(breaks = '5 day') +
+    geom_line(color = 'red') +
+    theme_classic() +
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1)
+    )
 
 
     
